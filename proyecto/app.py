70eb85f6f86e031aa91bf8ead7bb670e3ac36be7
Rocio <ralbarracintrab@gmail.com>
2025-09-04 12:19:19 -0300

    Base de datos conectada a Flask. Logramos mostrar todos los clientes junto a sus datos a traves de sql

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,36 +1,43 @@
+# NOTA PARA LAS CLASES CON DB CONEXION: EVE HACE UNA RUTAS y yo otras
 from flask import Flask, jsonify
 import mysql.connector
 from mysql.connector import Error
 
 app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(debug=True)
 
-# Función para obtener la conexión a la base de datos MySQL
+#-----------------------------------------------------------
+# Función para OBTENER la conexión a la base de datos MySQL
 def obtener_conexion():
     try:
         conexion = mysql.connector.connect(
-            host = "10.9.120.5",     
-            puerto = "3306",  
+            host = "10.9.120.5", 
+            port = 3306,    
             user = "accesorio",               # El usuario que usas en phpMyAdmin
-            password = "accesoio1234",  
+            password = "accesorio1234",  
             database = "accesorios"           # El nombre de la base de datos que usas en phpMyAdmin
         )
         if conexion.is_connected():
             print("Conexión exitosa a la base de datos")
             return conexion
-    except Error as e:
+        
+    except Error as e: # "e" = mysql.connector.Error, y contiene detalles sobre el error
         print(f"Error al conectar a la base de datos: {e}")
         return None
+#-----------------------------------------------------------
 
 
 # Ruta para obtener datos desde la base de datos
-@app.route('/usuarios', methods=['GET'])
+@app.route('/usuarios') # GET por defecto
 def obtener_usuarios():
     conexion = obtener_conexion() #Abrimos conexión
     if conexion is None:
         return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
-    
-    cursor = conexion.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM usuarios")  # Suponiendo que tienes una tabla 'usuarios'
+    #si
+
+    cursor = conexion.cursor(dictionary=True) #¿?
+    cursor.execute("SELECT * FROM clientes")  # Suponiendo que tienes una tabla 'usuarios'
     
     usuarios = cursor.fetchall()  # Obtiene todos los registros de la consulta
     
@@ -40,5 +47,5 @@ def obtener_usuarios():
     
     return jsonify(usuarios)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
