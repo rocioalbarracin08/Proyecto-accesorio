0f9fa4a9f949705c5f088179e7261151edb53fef
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-09-17 10:42:15 -0300

    agregue una imagen, modifique css para que ocupara toda la pantalla

--- proyecto/accesoriosPage/src/App.jsx
+++ proyecto/accesoriosPage/src/App.jsx
@@ -1,26 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-
-
-  return 
-  (
-    <div>
-      <div>
-        <h1>Bienvenido</h1>
-
-        <input type="text" />
-        <input type="text" />
-        
-        <button>Login</button>
-        
-        <a className= "link">¿Perdiste tu contraseña?</a>
-        <a >¿No tienes cuenta? Registrate</a> 
-      </div> 
-    <a >Volver</a>
+  return (
+    <div className="background-image">
+      <h1>Bienvenido</h1>
+      <input type="text" />
+      <input type="text" />
+      <button>Login</button>
+      <a className="link">¿Perdiste tu contraseña?</a>
+      <a>¿No tienes cuenta? Regístrate</a>
+      <a>Volver</a>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
