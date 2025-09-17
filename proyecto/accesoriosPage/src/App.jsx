8bd08c235cf92cfe81433f10ee517f96b2f28abf
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-09-17 10:58:45 -0300

    commit para hacer el pull

--- proyecto/accesoriosPage/src/App.jsx
+++ proyecto/accesoriosPage/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
 
+  return (
+    <div className="background-image">
+      <h1>Bienvenido</h1>
+      <input type="text" />
+      <input type="text" />
+      <button>Login</button>
+      <a className="link">¿Perdiste tu contraseña?</a>
+      <a>¿No tienes cuenta? Regístrate</a>
+      <a>Volver</a>
+    </div>
+  );
 
   return 
   (
     <Functions/>
   )
+
 }
 
-export default App
+export default App;
