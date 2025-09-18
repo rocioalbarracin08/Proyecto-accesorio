be691fc8c640df89498254ace397cbda63fe3f48
ana <anna.cristinna.20082gmail.com>
2025-09-18 11:40:07 -0300

    Realizo el archivo .venv

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,22 +1,24 @@
 # NOTA PARA LAS CLASES CON DB CONEXION: EVE HACE UNA RUTAS y yo otras
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv 
+import os
+
+load_dotenv() #Libreria que lee el archivo .env
 
 app = Flask(__name__)
-if __name__ == '__main__':
-    app.run(debug=True)
 
 #-----------------------------------------------------------
 # Función para OBTENER la conexión a la base de datos MySQL
 def obtener_conexion():
     try: #Código a probar
         conexion = mysql.connector.connect(
-            host = "10.9.120.5", 
+            host = os.getenv("DB_HOST"), 
             port = 3306,    
-            user = "accesorio",               # El usuario que usas en phpMyAdmin
-            password = "accesorio1234",  
-            database = "accesorios"           # El nombre de la base de datos que usas en phpMyAdmin
+            user = os.getenv("DB_USER"),               # El usuario que usas en phpMyAdmin
+            password = os.getenv("DB_PASSWORD"),  
+            database = os.getenv("DB_NAME")           # El nombre de la base de datos que usas en phpMyAdmin
         )
         if conexion.is_connected():
             print("Conexión exitosa a la base de datos")
@@ -26,7 +28,6 @@ def obtener_conexion():
         print(f"Error al conectar a la base de datos: {e}")
         return None
 #-----------------------------------------------------------
-
 # Ruta para obtener datos desde la base de datos
 @app.route('/usuarios') # GET por defecto
 def obtener_usuarios():
@@ -45,5 +46,73 @@ def obtener_usuarios():
     
     return jsonify(usuarios)
 
+@app.route("/api/categorias") #Para el boton de navegacón
+def categorias():
+    conexion =  obtener_conexion()
+    if conexion is None:
+        return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
+    cursor = conexion.cursor(dictionary=True)  # Para devolver como diccionarios
+    cursor.execute("SELECT categoria FROM categoria")  # Ajusta según tu tabla
+    
+    categorias = cursor.fetchall()  # Lista de dicts con las categorías
+    
+    cursor.close()
+    conexion.close()
+    return jsonify(categorias)
+
+@app.route("/api/carrito/<int:carrito_id>/agregar", methods = ['POST'])
+def carrito(carritoId):
+    
+    # request.get_json() convierte el body JSON del fetch en un diccionario de Python
+    data = request.get_json()
+
+    # .get("clave") accede a un valor dentro de ese diccionario
+    producto_id = data.get("producto_id")  # viene del JSON que mandaste en fetch
+    cantidad = data.get("cantidad")        # igual
+
+    return jsonify({
+        "producto_id": producto_id,
+        "cantidad": cantidad
+    })
+    
+@app.route("/api/accesorio")
+def accesorio():
+    conexion =  obtener_conexion()
+    if conexion is None:
+        return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
+    
+    cursor = conexion.cursor(dictonary=True)
+    cursor.execute("SELECT p.id, p.name, p.precio c.nombre from productos p JOIN categoria c ON p.id_categoria = c.id_category")
+
+    accesorio = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+
+    return jsonify(accesorio),200
+
+#@app.route("/api/register")
+#def registrarse():
 
 
+#@app.route("/api/login", methods=('GET', 'POST'))
+#def login():
+    
+@app.route("/api/Agregar")
+def agregarProductos():
+    try:
+        conexion =  obtener_conexion()
+        if conexion is None:
+            return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
+
+        cursor = conexion.cursor(dictonary=True)
+        cursor.execute("INSERT INTO productos (name, id_categoria, precio) VALUES (?, ?, ?)")
+
+        cursor.close()
+        conexion.close()
+        
+        return True
+    except 
+
+if __name__ == '__main__':
+    app.run(debug=True)
