6923cae343f66ad26e8bff7129104e57f867454b
Rocio <ralbarracintrab@gmail.com>
2025-09-24 13:53:53 -0300

    push de prueba

--- proyecto/accesoriosPage/src/components/piePagina/PiePag.jsx
+++ proyecto/accesoriosPage/src/components/piePagina/PiePag.jsx
@@ -3,15 +3,30 @@ import "./pie.css"
 export function PiePagina(){
     return(
     <footer>
-        <h4>CONTACTÁNOS</h4>
-        <p>accesorios@gmail.com</p>
-        <p>1132425262</p>
-        <h4>Redes sociales</h4>
-        <ul className="iconos">
-            <li><a href="instangram.com"><img src="/logos/igIcono.jpeg" alt="" /></a></li>
-            <li><a href="tiktok.com"><img src="/logos/tiktokIcono.png" alt="" /></a></li>
-            <li><a href="twiter.com"><img src="/logos/twiterIco.png" alt="" /></a></li>
-        </ul>
+        <div>
+        <div className="contacto">
+            <h4>CONTACTÁNOS</h4>
+            <p>accesorios@gmail.com</p>
+            <p>1132425262</p>
+            <h4>Redes sociales</h4>
+            <ul className="iconos">
+                <li>
+                    <a href="instangram.com"><img src="/logos/igIcono.jpeg" alt="" /></a>
+                </li>
+                <li>
+                    <a href="tiktok.com"><img src="/logos/tiktokIcono.png" alt="" /></a>
+                </li>
+                <li>
+                    <a href="twiter.com"><img src="/logos/twiterIco.png" alt="" /></a>
+                </li>
+            </ul>
+        </div>
+        <div className="compañia">
+            <h4>Compañia</h4>
+            <a href="#">¿Quiénes somos?</a>
+            <a href="#">¿Qué hacemos?</a>
+        </div>
+        </div>
         <p>© 2025 Accesorios | Todos los derechos reservados.</p>
     </footer>
     )
