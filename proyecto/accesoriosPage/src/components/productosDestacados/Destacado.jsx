d36c430d142e655b6dec40b9cd20b181c6c5e5e0
Rocio <ralbarracintrab@gmail.com>
2025-09-24 20:26:21 -0300

    commit para cambiar de rama

---
+++ proyecto/accesoriosPage/src/components/productosDestacados/Destacado.jsx
@@ -0,0 +1,14 @@
+export function Destacados(){
+
+
+    return(
+        <section>
+            <img src="/accesorios.jpg" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </section>
+    )
+    }
