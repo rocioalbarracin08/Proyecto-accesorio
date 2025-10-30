8e8d4aff06e4d1845850f0f29c1b6995f6318c0b
Rocio Albarracin <ralbarracintrab@gmail.com>
2025-10-30 12:35:17 -0300

    Faltan cambios para el navegador (cuestiones de estilo)

--- proyecto/client-react/src/components/productosDestacados/Destacado.jsx
+++ proyecto/client-react/src/components/productosDestacados/Destacado.jsx
@@ -4,12 +4,12 @@ export function Destacado(){
     //Mover fotos a productos
     return(
         <section className="seccion-img">
-            <img src="/accesorios.jpg" alt="broche" />
-            <img src="/brocheFoto.jpg" alt="foto2" />
-            <img src="/broche.jpeg" alt="foto3" />
-            <img src="/broche2.jpeg" alt="foto4" />
-            <img src="/foto.jpg" alt="foto5" />
-            <img src="/broche3.jpeg" alt="foto6" />
+            <img src="/productos/accesorios.jpg" alt="broche" />
+            <img src="/productos/vinchaFoto.jpg" alt="foto2" />
+            <img src="/productos/brochesRectangulares.jpeg" alt="foto3" />
+            <img src="/productos/brochesPerlados.jpg" alt="foto4" />
+            <img src="/productos/brocheFlor.jpeg" alt="foto5" />
+            <img src="/productos/vinchaConFlor.jpeg" alt="foto6" />
         </section>
     )
     }
